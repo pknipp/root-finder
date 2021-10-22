6e0509dff3fa2874767ef959a2976531a70069ac
Peter Knipp <peteraknipp@gmail.com>
2021-10-22 15:35:50 -0400

    Minor changes to instructions.

--- back/helper.py
+++ back/helper.py
@@ -13,15 +13,15 @@ instructions = [ \
     { \
         "polynomial format" :  \
             [ \
-                "Each of the polynomial's coefficients may be represented as an integer or decimal, but not a fraction (because '/' has special meaning in a URL).",\
+                "Each of the polynomial's coefficients may be represented as an integer or decimal but not as a fraction, because '/' has special meaning in a URL.",\
                 "Your variable must be a string which starts with a letter (upper- or lowercase) or underscore and which - if multi-character - otherwise contains only letters, underscores, or digits.",\
                 "Represent the product of a coefficient and a variable in the usual sequence: coefficient before variable.",    \
                 "Represent the multiplication operation either by a '*' or in an implied manner (ie with nothing separating the coefficient and the variable).",\
-                "Represent powers using either '^' or '**' notation.  E.g., 'x cubed' would be either 'x^3' or 'x**3'.", \
+                "Represent 'x squared' either as 'x**2' (preferably) or 'x^2' (OK) but not as 'x*x'. Do likewise for higher powers.", \
                 "You need not represent the absolute value of a coefficient if it equals 1.  For instance you may type 'x' instead of '1x' or '1*x', or '-x' instead of '-1x' or '-1*x'.",\
                 "You need not type the polynomial's terms in any particular order (such as largest power first or last).",\
-                "You need not include any terms in the polynomial for which the coefficient is zero.  For instance you may write '4x^2-9' instead of '4x^2+0x-9'.",\
+                "You need not include any terms in the polynomial for which the coefficient is zero.  For instance you may write '4x**2-9' instead of '4x**2+0x-9'.",\
                 "Spaces in your formula are optional, but be forewarned that their use will make the address less easy to read after you hit 'RETURN', because each space will be replaced by '%20'.", \
             ]   \
     }   \
-]   
+]
